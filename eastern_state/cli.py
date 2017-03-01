8d23215246f1d13d7714b48ec049a6ab319cb5ae
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-03-01 15:45:15 -0500

    Support piping

--- eastern_state/cli.py
+++ eastern_state/cli.py
@@ -4,7 +4,7 @@ import yaml
 import click
 import boto3
 
-from .yaml_utils import Loader, Dumper
+from .yaml_utils import Loader, Dumper, UnencryptedTag, EncryptedTag
 from .encryption import encrypt_file, decrypt_file
 
 ## TODO: create git hooks to prevent unencrypted creds from being committed
@@ -33,7 +33,7 @@ def encrypt(filename):
     sys.stdout.write(output)
 
 @main.command(help='Decrypts values tagged `!encrypted` and overwrites the file.')
-@click.option('-f','--filename', default='env.yml', help='Environments YAML file path')
+@click.option('-f','--filename', help='Environments YAML file path')
 def decrypt(filename):
     if filename != None:
         file = open(filename)
@@ -51,32 +51,32 @@ def decrypt(filename):
 
     sys.stdout.write(output)
 
-@main.command(help='Uploads environment files to S3')
-@click.option('-f','--filename', default='env.yml', help='Environments YAML file path')
+@main.command(help='Uploads environment config file to S3')
+@click.option('-f','--filename', help='Environments YAML file path')
 def upload(filename):
     if filename != None:
         file = open(filename)
     else:
         file = sys.stdin
 
-    env_file = yaml.load(file, Loader)
+    raw_file = file.read().encode('utf-8')
 
-    if filename != None:
-        file.close()
+    env_file = yaml.load(raw_file, Loader)
 
     client = boto3.client('s3')
-
-    file.seek(0)
 
     client.put_object(
         Bucket=env_file['bucket'],
         Key=env_file['name'],
-        Body=file.read().encode('utf-8'))
+        Body=raw_file)
 
-@main.command(help='Uploads environment files to S3')
+    if filename != None:
+        raw_file.close()
+
+@main.command(help='Downloads environment config file from S3')
 @click.argument('bucket')
 @click.argument('name')
-@click.option('-f','--filename', default='env.yml', help='Path to save environments YAML')
+@click.option('-f','--filename', help='Path to save environments YAML')
 def download(bucket, name, filename):
     client = boto3.client('s3')
 
@@ -91,3 +91,30 @@ def download(bucket, name, filename):
             file.write(output)
     else:
         sys.stdout.write(output)
+
+@main.command(help='Outputs bash environment exports')
+@click.argument('env')
+@click.option('-f','--filename', help='Environments YAML file path')
+def exports(env, filename):
+    if filename != None:
+        file = open(filename)
+    else:
+        file = sys.stdin
+
+    env_file = yaml.load(file, Loader)
+
+    if filename != None:
+        file.close()
+
+    variables = env_file['environments'][env]['variables']
+
+    def get_value(value):
+        if isinstance(value, UnencryptedTag) or isinstance(value, EncryptedTag):
+            return value.value
+        return value
+
+    output = ''
+    for var in variables:
+        output += 'export {}={}\n'.format(var, get_value(variables[var]))
+
+    sys.stdout.write(output)
